ee157715301128bcb1fed851d624ac8d3d6cfe16
devrilynn <130400488+devrilynn@users.noreply.github.com>
2024-07-31 21:35:04 -0700

    Create NavBar.js
    nav bar

---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavBar.css'; 
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <Link to="/" div className="navbar-brand">
+        <h3>ElixirMixer</h3>
+      </Link>
+      <div className="navbar-links">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
