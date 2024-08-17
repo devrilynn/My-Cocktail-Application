facaaeff0b0a9bfe6bb205713f006c84acc4d77c
devrilynn <130400488+devrilynn@users.noreply.github.com>
2024-08-16 19:41:54 -0700

    Update Recipes.js
    Include multiple ingredients filter search

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,28 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import NavBar from './NavBar';
-import { fetchRecipesByIngredient } from './IngredientsApi';
+import { fetchRecipesByIngredient, fetchRecipesByIngredients } from './IngredientsApi';
 import Masonry from 'react-masonry-css';
 import './Recipes.css';
 
+// Recipes component
 const Recipes = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const [recipes, setRecipes] = useState([]);
-    const [loading, setLoading] = useState(true);               
-    const ingredient = location.state?.ingredient;
+    const [loading, setLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const ingredient = useMemo(() => location.state?.ingredient, [location.state?.ingredient]);
+    const ingredients = useMemo(() => location.state?.ingredients || (ingredient ? [ingredient] : []), [location.state?.ingredients, ingredient]);
 
     useEffect(() => {
-        if (ingredient) {
-            fetchRecipesByIngredient(ingredient, (data) => {
-                setRecipes(data);
-                setLoading(false);              // Set loading to false after data is fetched
-            });
+        if (ingredients.length > 0) {
+            if (ingredients.length > 1) {
+                fetchRecipesByIngredients(ingredients, (data) => {
+                    handleFetchResults(data);
+                });
+            } else if (ingredients.length === 1) {
+                fetchRecipesByIngredient(ingredients[0], (data) => {
+                    handleFetchResults(data);
+                });
+            }
         } else {
             navigate('/');
         }
-    }, [ingredient, navigate]);
+    }, [ingredients, navigate]);
 
+    // Function to handle the results from the API call.
+    const handleFetchResults = (data) => {
+        if (data && data.length > 0) {
+            setRecipes(data);
+            setErrorMessage('');
+        } else {
+            setErrorMessage(`No recipes found with the selected ingredients.`);
+            setRecipes([]);
+        }
+        setLoading(false);
+    };
+
+    // Function to set columns for display of recipes
     const breakpointColumnsObj = {
         default: 4,
         1100: 3,
@@ -33,30 +55,32 @@ const Recipes = () => {
     return (
         <div>
             <NavBar />
-            <div className="back-button">
-                <button onClick={() => navigate('/')}>Back to Ingredients</button>
+            <div className="header-container">
+              <div className="back-button">
+                <button className="back-button" onClick={() => navigate('/')}>Back to Ingredients</button>
+              </div>
+              <h1 className="header-title">Recipes with {ingredients.join(', ')}</h1>
             </div>
             <div className="container">
-                <h1>Recipes with {ingredient}</h1>
-                {loading ? (                        // Conditional rendering based on loading state
-                    <p>Loading...</p> 
-                ) : recipes.length > 0 ? (          // If recipes are available
+                {loading ? (
+                    <p>Loading...</p>
+                ) : errorMessage ? (
+                    <p>{errorMessage}</p>
+                ) : (
                     <Masonry
                         breakpointCols={breakpointColumnsObj}
                         className="masonry-grid"
                         columnClassName="masonry-grid_column"
                     >
-                        {recipes.map(recipe => ( 
-                            <Link to={`/RecipeDetails/${recipe.idDrink}`} state={{ from: location.pathname, ingredient: ingredient }} key={recipe.idDrink}>
+                        {recipes.map(recipe => (
+                            <Link to={`/RecipeDetails/${recipe.id}`} state={{ from: location.pathname, ingredients }} key={recipe.id}>
                                 <div className="masonry-grid_item">
-                                    <img src={recipe.strDrinkThumb} alt={recipe.strDrink} className="ingredient-image" />
-                                    <p>{recipe.strDrink}</p>
+                                    <img src={recipe.image} alt={recipe.name} className="ingredient-image" />
+                                    <p>{recipe.name}</p>
                                 </div>
                             </Link>
                         ))}
                     </Masonry>
-                ) : (
-                    <p>No recipes found for {ingredient}</p> 
                 )}
             </div>
         </div>
