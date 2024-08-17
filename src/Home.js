3c5fba2eb77b61131e8eeddc1fad5541c4020c29
devrilynn <130400488+devrilynn@users.noreply.github.com>
2024-08-16 19:33:40 -0700

    Update Home.js
    Updated home to include filtering by multiple ingredients

--- src/Home.js
+++ src/Home.js
@@ -1,46 +1,63 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBar from './NavBar';
+import Filter from './Filter'; 
 import IngredientThumbnails from './IngredientThumbnails';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFilter, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import './index.css'; 
 
 const Home = () => {
-  const [ingredient, setIngredient] = useState('');
+  const [filteredIngredients, setFilteredIngredients] = useState([]);
+  const [showFilter, setShowFilter] = useState(false);
   const navigate = useNavigate();
 
-  // Function to handle searches
+  // Function to handle changes in the selected ingredients from the filter
+  const handleFilterChange = (ingredients) => {
+    setFilteredIngredients(ingredients);
+  };
+
+  // Function for handling the search button
   const handleSearch = () => {
-    if (ingredient) {
-      navigate('/recipes', { state: { ingredient } });
+    if (filteredIngredients.length > 0) {
+      navigate('/recipes', { state: { ingredients: filteredIngredients } });
     }
   };
 
-  // Function to handle ingredient selection from thumbnails
+  // Function handle selecting a single ingredient
   const handleIngredientSelect = (selectedIngredient) => {
     navigate('/recipes', { state: { ingredient: selectedIngredient } });
+  };
+
+  // Function to toggle the filter button
+  const toggleFilter = () => {
+    setShowFilter(!showFilter);
   };
 
   return (
     <div>
       <NavBar />
       <div className="container">
-        <h1>Welcome to ElixirMixer!</h1>
-        <p>Find cocktail recipes based on the ingredients you have on hand.</p>
-        <input
-          type="text"
-          value={ingredient}
-          className="search-box"
-          onChange={(e) => setIngredient(e.target.value)}
-          onKeyDown={(event) => {
-            if (event.key === 'Enter') {
-              handleSearch();
-            }
-          }}
-          placeholder="Enter an ingredient on hand..."
-        />
-        <button onClick={handleSearch}>Search</button>
-        <h2>Browse Cocktails by Ingredient</h2>
-        <IngredientThumbnails onIngredientSelect={handleIngredientSelect} />      
+        <h1>Browse Cocktails by Ingredient</h1>
+        <div className="filter-search-container">
+          <button onClick={toggleFilter} className="filter-button">
+            {showFilter ? 'Hide Filter' : 'Filter By Multiple Ingredients'}
+            &nbsp;<FontAwesomeIcon icon={faFilter} />
+          </button>
+        </div>
+        {showFilter && (
+          <>
+            <Filter onFilterChange={handleFilterChange} />
+            {filteredIngredients.length > 0 && (
+              <div className="search-button-container">
+                <button onClick={handleSearch} className="search-button">
+                  Search Cocktails &nbsp;<FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+              </div>
+            )}
+          </>
+        )}
+        <IngredientThumbnails onIngredientSelect={handleIngredientSelect} />
       </div>
     </div>
   );
